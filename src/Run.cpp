a2aeb47dd355d0c98b8dc2a55392999ba05ad2f9
Michael Fildstein Id:309161594 <Michael Fildstein Id:309161594@monster>
2014-11-14 17:33:42 +0200

    got all lines to appear

--- src/Run.cpp
+++ src/Run.cpp
@@ -11,7 +11,12 @@ int main(int argc, char *argv[]) {
 
 	// Initiation a link with a null pointer.
 	Link first = Link("This is a first link",NULL);
+	Link* pFirst = &first;
+	Link copiedLink = Link(first);
 
+	List aList = List();
+	List bList = List(aList);
+	aList = bList;
 
 	return 0;
 
