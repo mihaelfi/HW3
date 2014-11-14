f7533695846eebc95a3d65a03221469226038298
Michael Fildstein Id:309161594 <Michael Fildstein Id:309161594@monster>
2014-11-14 17:20:18 +0200

    Got it to print the first line by initaitng a link with a null pointer.

--- src/Run.cpp
+++ src/Run.cpp
@@ -1,11 +1,16 @@
 
 #include "../include/LinkedList.h"
-
+using namespace std;
 
 
 
 int main(int argc, char *argv[]) {
 
+//	Link null = Link();
+
+
+	// Initiation a link with a null pointer.
+	Link first = Link("This is a first link",NULL);
 
 
 	return 0;
